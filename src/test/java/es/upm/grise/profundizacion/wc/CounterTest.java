310a95759aaf60a548dc955f7e9436e7db2ccf9f
carloslopllo <carlos.lopez.llorente@alumnos.upm.es>
2024-10-16 17:17:53 +0200

    test .jnose

--- src/test/java/es/upm/grise/profundizacion/wc/CounterTest.java
+++ src/test/java/es/upm/grise/profundizacion/wc/CounterTest.java
@@ -37,25 +37,25 @@ public class CounterTest {
 
     @Test
     public void test_getNumberCharacters() {
-        int[] e = {210, 229, 774};
-        int[] a = {counter[0].getNumberCharacters(), counter[1].getNumberCharacters(), counter[2].getNumberCharacters()};
+        int e = 210;
+        int a = counter[0].getNumberCharacters();
 
-        assertEquals(e[0], a[0], "test_getNumberCharacters : expected: 210, actual: , file test_file/a.txt");
+        assertEquals(e, a, "test_getNumberCharacters : expected: 210, actual: , file test_file/a.txt");
     }
 
     @Test
     public void test_getNumberLines() {
-        int[] e = {1, 2, 24};
-        int[] a = {counter[0].getNumberLines(), counter[1].getNumberLines(), counter[2].getNumberLines()};
+        int e = 1;
+        int a = counter[0].getNumberLines();
 
-        assertEquals(e[0], a[0], "test_getNumberLines : expected: 1, actual: , file test_file/a.txt");
+        assertEquals(e, a, "test_getNumberLines : expected: 1, actual: , file test_file/a.txt");
     }
 
     @Test
     public void test_getNumberWords() {
-        int[] e = {35, 47, 143};
-        int[] a = {counter[0].getNumberWords(), counter[1].getNumberWords(), counter[2].getNumberWords()};
+        int e = 35;
+        int a = counter[0].getNumberWords();
 
-        assertEquals(e[0], a[0], "test_getNumberWords : expected: 35, actual: , file test_file/a.txt");
+        assertEquals(e, a, "test_getNumberWords : expected: 35, actual: , file test_file/a.txt");
     }
 }
